e56ec5c31d478b01b0983a2554ee8b981dcd222f
CynthiaJerono <cynthiajerono1@gmail.com>
2025-08-06 15:58:06 +0300

    made changes on answers.sql and exported the results grid and saved it.

--- answers.sql
+++ answers.sql
@@ -1,3 +1,20 @@
+-- Create a new database called salesdb
+CREATE DATABASE salesdb;
 
-
+-- show me you created the database
 SHOW DATABASES;
+
+-- for me to drop the database demo i need to create it
+CREATE DATABASE DEMO;
+
+-- Actually show me that you created it
+SHOW DATABASES;
+
+-- Drop (delete) the database called demo
+DROP DATABASE demo;
+
+-- Show all databases
+SHOW DATABASES;
+
+
+
